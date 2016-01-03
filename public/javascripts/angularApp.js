757e67a69041fd8409457fd77be43800f3827348
ggloege <gabegloege@gmail.com>
2016-01-02 21:47:54 -0500

    First upload
    This is the full directory minus the bin and node modules

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,92 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl',
+				resolve: {
+					postPromise: ['posts', function(posts) {
+						return posts.getAll();
+					}]
+				}
+			})
+
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+	}
+]);
+
+app.factory('posts', ['$http', function() {
+	var o = {
+		posts: []
+	};
+
+	o.getAll = function() {
+		return $http.get('/posts').success(function(data) {
+			angular.copy(data, o.posts);
+		});
+	};
+
+	o.create = function(post) {
+		return $http.post('/posts', post).success(function(data) {
+			o.posts.push(data);
+		});
+	};
+
+	return o;
+}]);
+
+app.controller('MainCtrl', [
+	'$scope',
+	'posts',
+	function($scope, posts) {
+		$scope.posts = posts.posts;
+		$scope.addPost = function() {
+			if(!$scope.title || $scope.title === '') { return; }
+			posts.create({
+				title: $scope.title, 
+				link: $scope.link
+			});
+			$scope.title = '';
+			$scope.link = '';
+		};
+		$scope.incrementUpvotes = function(post) {
+			post.upvotes++;
+		}
+
+	}
+
+]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts) {
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function() {
+			if($scope.body === '') {return;}
+			$scope.post.comments.push({
+				author: 'user',
+				body: $scope.body,
+				upvotes: 0
+			})
+			$scope.body = '';
+		}
+		$scope.incrementUpvotes = function(comment) {
+			post.comments.upvotes++;
+		}
+
+	}
+]);
